46695c564faede97f4581f24362745a5197cd51d
nauosika <nauosikaforwork@gmail.com>
2021-09-11 19:20:31 +0800

    shoulda_mathcers test ok

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,8 +1,13 @@
 class Role < ApplicationRecord
+  has_one :skill
+
   belongs_to :user
+
   has_many :swords
+
   has_many :boxes
   has_many :items, through: :boxes
+
   validates :name , presence: true, length: { maximum: 10 }
   validates :job , presence: true, length: { minimum: 2 }
   validates :age , presence: true, numericality: { only_integer: true }
